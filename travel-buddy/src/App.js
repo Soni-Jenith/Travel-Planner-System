07afef8010470d57ae0d1e4422a13f7a01eff7ed
JENITH-SONI <128300332+JENITH-SONI@users.noreply.github.com>
2024-08-21 16:10:51 +0530

    updated site

--- travel-buddy/src/App.js
+++ travel-buddy/src/App.js
@@ -1,12 +1,42 @@
-
+import React, { useEffect, useRef } from 'react';
+import {gsap} from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import './App.css';
+import HomeSection from './components/BeforeLogIn/HomeSection';
+import Features from './components/BeforeLogIn/Features';
+import Section3 from './components/BeforeLogIn/Section3';
+import Section4 from './components/BeforeLogIn/Section4';
+import CustomiseTrips from './components/BeforeLogIn/CustomiseTrips';
+import Footer from './components/BeforeLogIn/Footer';
+
+gsap.registerPlugin(ScrollTrigger);
+
 
 function App() {
-  return (
-    <div className="App">
-<h1>test</h1>
+ 
   
-    </div>
+
+  return (
+    
+
+    <body id='body' data-scroll-container>
+
+      <HomeSection className="home section"/>
+
+      <Features data-scroll data-scroll-speed="4" />
+
+      <Section3 />
+
+      <Section4 />
+
+      <CustomiseTrips/>
+      
+      <Footer/>
+      
+      
+      
+    </body>
+    
   );
 }
 
